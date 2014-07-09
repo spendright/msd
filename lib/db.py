17d45a221b3887cd2638de588fad3c875122b955
David Marin <dm@davidmarin.org>
2014-07-09 11:57:27 -0700

    factored out most code into lib/

---
+++ lib/db.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright 2014 David Marin
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Download and open databases."""
+
+import logging
+import sqlite3
+from os import environ
+from os import rename
+from os.path import exists
+from tempfile import NamedTemporaryFile
+from urllib import urlencode
+from urllib2 import urlopen
+
+
+DB_TO_URL = {
+    'campaigns': 'https://morph.io/spendright-scrapers/campaigns/data.sqlite',
+}
+
+CHUNK_SIZE = 1024
+
+
+log = logging.getLogger(__name__)
+
+
+def open_db(name):
+    if not hasattr(open_db, '_name_to_db'):
+        open_db._name_to_db = {}
+
+    if name not in open_db._name_to_db:
+        filename = name + '.sqlite'
+        if not exists(filename):
+            if 'MORPH_API_KEY' not in environ:
+                raise ValueError(
+                    'Must set MORPH_API_KEY to download {} db'.format(name))
+
+            url = DB_TO_URL[name] + '?' + urlencode(
+                {'key': environ['MORPH_API_KEY']})
+
+            log.info('downloading {} -> {}'.format(url, filename))
+            download(url, filename)
+        else:
+            log.info('opening local copy of {}'.format(filename))
+
+        open_db._name_to_db[name] = sqlite3.connect(filename)
+
+    return open_db._name_to_db[name]
+
+
+def download(url, dest):
+    with NamedTemporaryFile(prefix=dest + '.tmp.', dir='.', delete=False) as f:
+        src = urlopen(url)
+        while True:
+            chunk = src.read(CHUNK_SIZE)
+            if not chunk:
+                break
+            f.write(chunk)
+
+        f.close()
+        rename(f.name, dest)
