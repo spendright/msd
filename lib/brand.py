0c0dfe5df24e744149d39b3fb17e7900494035d8
David Marin <dm@davidmarin.org>
2014-07-17 10:28:10 -0700

    whoops, forgot to add this

---
+++ lib/brand.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright 2014 David Marin
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Utilities for merging brand information."""
+from .db import select_campaign_brands
+
+
+def get_brand_to_row(keys):
+    """Given keys, a list of (campaign_id, company), return a dictionary
+    mapping canonical brand name to information about that brand."""
+
+    # we'll eventually need the ability to choose a master list
+    # of brands (so we can ignore brands that a campaign wrongly
+    # assigns to a company), and be able to handle variant spellings
+    # (e.g. Liquid-Plumr/LiquidPlumr)
+
+    # We'll also need to catch when a brand has been rated but isn't
+    # in the master list (include it but issue a warning)
+    brand_to_row = {}
+
+    for campaign_id, company in sorted(keys):
+        brand_rows = select_campaign_brands(campaign_id, company)
+        for brand_row in brand_rows:
+            del brand_row['campaign_id']
+            brand = brand_row['brand']
+
+            if brand in brand_to_row:
+                brand_to_row[brand].update(brand_row)
+            else:
+                brand_to_row[brand] = brand_row
+
+    return brand_to_row
