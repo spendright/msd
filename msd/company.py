31a313d6da3499702c0ebee93a43e1200d8385cc
David Marin <dm@davidmarin.org>
2015-07-31 13:47:18 -0700

    added matching variant function

--- msd/company.py
+++ msd/company.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from functools import lru_cache
 from logging import getLogger
 
 from .company_data import BAD_COMPANY_VARIANTS
 from .company_data import COMPANY_CORRECTIONS
 from .company_data import COMPANY_DISPLAY_REGEXES
+from .company_data import COMPANY_MATCHING_REGEXES
 from .company_data import COMPANY_TYPE_CORRECTIONS
 from .company_data import COMPANY_TYPE_RE
 from .company_data import UNSTRIPPABLE_COMPANIES
@@ -40,13 +42,14 @@ def build_scraper_company_map_table(output_db, scratch_db):
     pass
 
 
-
-def get_company_variants(company):
-    return set(v for v in _yield_company_variants(company)
+@lru_cache(maxsize=1)
+def get_company_display_variants(company):
+    """Get a set of possible ways to display company name."""
+    return set(v for v in _yield_company_display_variants(company)
                if len(v) > 1)
 
 
-def _yield_company_variants(company):
+def _yield_company_display_variants(company):
     company = COMPANY_CORRECTIONS.get(company) or company
 
     # if it's a name like Foo, Inc., allow "Foo" as a display variant
@@ -77,3 +80,26 @@ def _yield_company_variants(company):
             if variant not in BAD_COMPANY_VARIANTS:
                 yield variant
                 break
+
+
+def get_company_matching_variants(company):
+    """Get a set of all ways to match against this company. Some of
+    these may be too abbreviated to use as the company's display name."""
+    variants = get_company_display_variants()
+
+    # Match "The X Company", "X Company", "Groupe X"
+    for regex in COMPANY_MATCHING_REGEXES:
+        m = regex.match(company)
+        if m:
+            variant = m.group('company')
+            if variant not in BAD_COMPANY_VARIANTS:
+                variants.add(variant)
+                break
+
+    # split on slashes
+    for v in list(variants):
+        if '/' in v and not COMPANY_TYPE_RE.match(v):  # don't split A/S
+            variants.update((part.strip() for part in v.split('/')))
+
+    # remove short/empty matches
+    return set(v for v in variants if len(v) > 1)
