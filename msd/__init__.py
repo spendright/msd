4fc411893f24b8adba4077908f40a9e5cfd686fa
David Marin <dm@davidmarin.org>
2016-02-08 16:16:14 -0800

    bumped version, updated CHANGES.txt, updated copyright year

--- msd/__init__.py
+++ msd/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015 SpendRight, Inc.
+# Copyright 2015-2016 SpendRight, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = '0.1.2-dev'
+__version__ = '0.1.3'
--- msd/norm.py
+++ msd/norm.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014-2015 SpendRight, Inc.
+# Copyright 2014-2016 SpendRight, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
